75b4c382c3f037fb5e40fa55340f9ad6d06ac174
LeedsByExample <leeda.minkin@yahoo.com>
2023-10-23 19:57:06 -0500

    Completed steps 1 and 2 of Mars Rover.

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,20 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  
+  test("constructor sets command type", function() {
+    let value = "bite me";
+    let commandType = "get bent";
+    let command = new Command(commandType,value);
+    expect(command.commandType).toBe(commandType)
+  });
+
+  //checks that the constructor correctly sets the value property in the new object
+  test("constructor sets a value passed in as the 2nd argument", function() {
+    let value = "value this ass";
+    let commandType = "insert funny thing here";
+    let command = new Command(commandType,value);
+    expect(command.value).toBe(value)
+  });
 
 });
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,16 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    test("throws error if name is NOT passed into constructor as the first parameter", function() {
+        expect(function() {new Message();}).toThrow(new Error("Name required."));
+    });
 
+    test("constructer sets name", function() {
+        let message = new Message("please no more");
+        expect(message.name).toBe("please no more");
+    });
 });
+
+// it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+//     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+//   });
